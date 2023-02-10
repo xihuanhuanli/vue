aa1c81c2d3e2d3f9922e6c1024e5bb359dd70645
xihuanhuanli <105272253+xihuanhuanli@users.noreply.github.com>
2023-02-10 15:15:09 +0800

    粗略修改前端

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import request from '@/utils/request'
+
+  export function login(data) {
+    return request({
+      url: 'user/selectALL',
+      method: 'post',
+      data
+    })
+  }
+/**
+ * 获取用户历史订单信息
+ */
+export function login1(data) {
+    return request({
+      url: '/vue-admin-template/user/login',
+      method: 'post',
+      data
+    })
+  }
+/**
+ * 获取动态路由
+ */
+export function login2(data) {
+    return request({
+      url: '/vue-admin-template/user/login',
+      method: 'post',
+      data
+    })
+  }
