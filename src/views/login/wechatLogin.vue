88f6ac03a248c95cf911b29e5d918db2b979cef2
xihuanhuanli <105272253+xihuanhuanli@users.noreply.github.com>
2023-04-11 11:15:33 +0800

    test successs

--- src/views/login/wechatLogin.vue
+++ src/views/login/wechatLogin.vue
@@ -1,12 +1,33 @@
 <template>
     <div class="wxLogin">
-      <span class="title">微信扫一扫登录</span>
-      <img src="../../icons/img/test.png">
-      <p>
-        请先使用微信扫描二维码关注公众号
-      </p>
-      <img id="id" src="../../icons/img/2.png" height="200" width="200" style="visibility:hidden" >
-      <el-button  @click="myclose()">点击打开登录二维码</el-button>
+      <div style="margin-left:600px;margin-bottom: 25px;margin-top:30px;">
+        <span class="title" style="border:1px solid #000;font-family: Arial;
+    font-size: 18px;
+    line-height: 10px;
+    font-weight: bold;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 0 0 0 10px white;">微信扫一扫登录</span>
+      </div>
+      <div style="margin-left:572px;margin-top: 20px;margin-bottom: 25px;">
+        <img style="border:1px solid #000" src="../../icons/img/test.png">
+      </div>
+      <div style="margin-left:520px;margin-top: 20px;margin-bottom: 25px;">
+        <span class="title" style="border:1px solid #000;font-family: Arial;
+    font-size: 18px;
+    line-height: 10px;
+    font-weight: bold;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 0 0 0 10px white;"> 请先使用微信扫描二维码关注公众号</span>
+      </div>
+      <div style="margin-left:597px;margin-top: 20px;margin-bottom: 20px;">
+        <img  id="id" src="../../icons/img/2.png" height="150" width="150" style="border:1px solid #000;visibility:hidden" >
+      </div>
+      <div style="margin-left:590px;margin-top: 10px;margin-bottom: 10px;">
+        <el-button  id="btn" @click="myclose()" v-if="flag">点击打开登录二维码</el-button>
+      </div>
+      
     </div>
   </template>
   
@@ -17,13 +38,15 @@
     components: { },
     data () {
       return {
-        i:0
+        i:0,
+        flag:true,
       }
     },
     mounted () {
     },
     methods: {
       myclose(){
+        this.flag=false
         this.i=0
         var imgId = document.getElementById("id"); 
         imgId.style.visibility ="visible";
@@ -32,6 +55,7 @@
       myref(){
         var imgId = document.getElementById("id"); 
         imgId.style.visibility ="hidden";
+        this.flag=true;
         this.$message({
               type: "error",
               message:"二维码过期",
@@ -68,4 +92,11 @@
   }
   </script>
   <style>
+  .wxLogin{
+    height: 100%;
+    padding-top: 10px;
+    background: no-repeat url(../background/login-page-background.jpg);
+    background-size: cover;
+    overflow: hidden;
+  }
   </style>
